909e4efb5d45550384ed0d45819cb7b6f2e4b1b7
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-08-09 18:24:48 +0000

    setup mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- create database & user 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON dhbnb_dev_db.* TO 'hbnb_dev'@'localhost';
