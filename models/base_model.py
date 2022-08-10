71c0f131ea5f44e92b29a2fc88cf2e74d0f491a8
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-08-10 14:31:21 +0000

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -16,11 +16,12 @@ class BaseModel:
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
