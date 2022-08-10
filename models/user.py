8c19d67a9aa3fa6452b5cf83271455690f2c0d5b
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-08-10 14:32:44 +0000

    user

--- models/user.py
+++ models/user.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This is the user class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
+class User(BaseModel, Base):
+    """This is the class for user
+    Attributes
+    """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
