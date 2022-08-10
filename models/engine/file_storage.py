5baf3e3f864eaa7b50c82dedca40c707e0c4850b
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-08-10 12:44:25 +0000

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it's inside"""
+        try:
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
+        except (AttributeError, KeyError):
+            pass
