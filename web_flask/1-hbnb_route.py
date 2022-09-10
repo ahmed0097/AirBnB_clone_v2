975943110e06f74e36108ab285e4acbeabf23486
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-09-10 07:41:45 -0700

    hbnb route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,6 +12,7 @@ def hbnb():
     in the route page
     """
     return 'Hello HBNB!'
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_():
     """
@@ -19,5 +20,6 @@ def hbnb_():
     in the route page
     """
      return 'HBNB'
+
 if __name__ == "__main__": 
     app.run(host='0.0.0.0', port=5000)
